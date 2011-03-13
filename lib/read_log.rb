489cc9c5a28c2538b254d96d454ea5bb06735d98
Ginny Hendry <contact@ghendry.com>
2011-03-12 23:22:35 -0600

    read log

--- lib/read_log.rb
+++ lib/read_log.rb
@@ -1,1 +1,22 @@
-#read query logs
+require 'csv'
+
+module IndexTester
+  class ReadLog
+    def initialize(file_name)
+      @file_name = file_name
+      @content = read
+    end
+    def read
+      CSV.read(@file_name)
+    end
+    def query
+      @content.transpose[0]
+    end
+    def stack_trace
+      @content.transpose[1]
+    end
+    def timestamp
+      @content.transpose[2]
+    end
+  end
+end
--- test/log_test.rb
+++ test/log_test.rb
@@ -1,11 +1,14 @@
 require_relative '../lib/read_log'
 
-class Read_log < MiniTest::Unit::TestCase
+class TestReadLog < MiniTest::Unit::TestCase
   def setup
+    @reader = IndexTester::ReadLog.new('fixture/query.log')
   end
 
-  def test_basic
-    assert_equal 1, 1
+  def test_read
+    assert_equal "select * from orders;", @reader.query[0]
+    assert_equal "the stack trace", @reader.stack_trace[0]
+    assert_equal "2011-03-13", @reader.timestamp[0]
   end
 
 end
