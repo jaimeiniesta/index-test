30f4ea52a23b5078ba7684770f177d1236cc4af3
Ginny Hendry <contact@ghendry.com>
2011-03-12 22:38:13 -0600

    basic project setup

---
+++ test/log_test.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/read_log'
+
+class Read_log < MiniTest::Unit::TestCase
+  def setup
+  end
+
+  def test_basic
+    assert_equal 1, 1
+  end
+
+end
+
---
+++ test/run_tests.rb
@@ -0,0 +1,5 @@
+require 'minitest/unit'
+require 'mocha'
+require_relative 'log_test'
+
+MiniTest::Unit.autorun
