8fd10c543a84ebc1f4215f2f7252bd40df385ecc
Ginny Hendry <contact@ghendry.com>
2011-03-17 23:40:23 -0500

    basic db access

--- lib/read_log.rb
+++ lib/read_log.rb
@@ -1,22 +1,61 @@
 require 'csv'
+require 'active_record'
 
 module IndexTester
   class ReadLog
     def initialize(file_name)
       @file_name = file_name
-      @content = read
+      @content = load_csv
     end
-    def read
+    def load_csv
       CSV.read(@file_name)
     end
-    def query
-      @content.transpose[0]
+    def unique_log_entries
+      @content.inject({}) {|list, line| list[line[1]] = line; list}.values
     end
-    def stack_trace
-      @content.transpose[1]
+    def unique_selects
+      unique_log_entries.inject([]) {|list, entry| list << entry[0]; list}
     end
-    def timestamp
-      @content.transpose[2]
+  end
+  class HarvestData
+    def initialize(db, queries)
+      @db, @queries = db, queries
+    end
+
+    def tables
+      ['orders']
+      get_explains.map {|exp| exp.match(/^TABLE (\w+)/)[1]}.uniq
+    end
+
+    def get_record_counts
+      tables.inject({}) {|counts, table| counts[table] = @db.execute_count(table); counts}
+    end
+
+    def get_explains
+      @explains ||= @queries.inject([]) {|result,q| result << @db.execute_explain(q); result}
+    end    
+    
+    def missing_indexes
+      get_explains.map {|exp| exp.match(/INDEX/).nil?}
+    end
+    def scanned_counts
+      get_explains.map {|exp| md = exp.match(/\~(\d+) rows/) ? md[0] : 0}
+    end
+  end
+  class DataBase
+    def initialize
+      ActiveRecord::Base.establish_connection(
+        :adapter => 'sqlite3',
+        :database => '../db/test')
+    end
+    def connection
+      ActiveRecord::Base.connection
+    end
+    def execute_count(table)
+      connection.execute("select count(*) from #{table};")[0][0]
+    end
+    def execute_explain(query)
+      connection.execute("explain query plan #{query}")[0]['detail']
     end
   end
 end
--- test/log_test.rb
+++ test/log_test.rb
@@ -5,11 +5,30 @@ class TestReadLog < MiniTest::Unit::TestCase
     @reader = IndexTester::ReadLog.new('fixture/query.log')
   end
 
-  def test_read
-    assert_equal "select * from orders;", @reader.query[0]
-    assert_equal "the stack trace", @reader.stack_trace[0]
-    assert_equal "2011-03-13", @reader.timestamp[0]
+  def test_select
+    assert_equal 3, @reader.unique_log_entries.size
+    assert_equal 3, @reader.unique_selects.size
+    refute_equal @reader.unique_selects[0], @reader.unique_selects[1]
   end
-
 end
 
+class TestHarvestData < MiniTest::Unit::TestCase
+  def setup
+    db = IndexTester::DataBase.new
+    @queries = IndexTester::ReadLog.new('fixture/query.log').unique_selects
+    @harvester = IndexTester::HarvestData.new(db, @queries)
+  end
+
+  def test_table_sizes
+    assert_equal 1, @harvester.get_record_counts.size
+    assert_operator 0, :<, @harvester.get_record_counts['orders']
+  end
+  def test_explain
+    assert_equal 3,@harvester.get_explains.size
+    assert_equal '',@harvester.get_explains
+  end
+  def test_explain_detail
+    assert_equal [true, true, false], @harvester.missing_indexes
+    assert_equal [2,2,1], @harvester.scanned_counts
+  end
+end
