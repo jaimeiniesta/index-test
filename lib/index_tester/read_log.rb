b3eb8cab33a6fae13edc67de0ffbe13417ab9ff2
Jaime Iniesta <jaimeiniesta@gmail.com>
2011-03-25 21:27:46 +0100

    Refactor read_log.rb

--- lib/index_tester/read_log.rb
+++ lib/index_tester/read_log.rb
@@ -4,19 +4,20 @@ module IndexTester
   class ReadLog
     def initialize(file_name)
       @file_name = file_name
-      @content = load_csv
-    end
-
-    def load_csv
-      CSV.read(@file_name)
     end
 
     def unique_log_entries
-      @content.inject({}) {|list, line| list[line[1]] = line; list}.values
+      content.inject({}) {|list, line| list[line[1]] = line; list}.values
     end
 
     def unique_selects
       unique_log_entries.inject([]) {|list, entry| list << entry[0]; list}
+    end
+
+    private
+
+    def content
+      @content ||= CSV.read(@file_name)
     end
   end
 end
